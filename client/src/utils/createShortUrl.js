ecff38c75f73c15533f84556ae7a78ff4ac04d25
HaxagonusD <jquezad1@binghamton.edu>
2021-12-12 13:04:51 -0500

    Should work on heroku now

--- client/src/utils/createShortUrl.js
+++ client/src/utils/createShortUrl.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 const createShortUrl = async (longUrl) => {
-  const response = await axios.post(
-    process.env.REACT_APP_BASE_URL_DEV + "api/v1/shorturl",
-    {
-      url: longUrl,
-    }
-  );
+  const baseUrl =
+    process.env.NODE_ENV === "production"
+      ? process.env.REACT_APP_BASE_URL_PROD
+      : process.env.REACT_APP_BASE_URL_DEV;
+  const response = await axios.post(baseUrl + "api/v1/shorturl", {
+    url: longUrl,
+  });
   return response.data;
 };
 
